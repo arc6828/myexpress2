96b1c4346795f154f1b5ed1f4796ed6d0339d40a
Chavalit Koweerawong <chavalit.kow@gmail.com>
2025-08-29 09:01:46 +0700

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const supabase = createClient(
 
 // Init Gemini
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash-lite" });
 
 app.get("/", (req, res) => {
   res.send("hello world, Chavalit");
